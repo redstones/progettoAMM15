f067b5342574cf3130f48e8ca2af2a424ee94340
redstones <massilepori@gmail.com>
2016-05-10 11:07:08 +0200

    Update master.php

--- php/view/master.php
+++ php/view/master.php
@@ -43,7 +43,7 @@ if (!$vd->isJson()) {
 			<header>
 				<div class="width" id="title">
 					<h1>
-						<img src="../images/small_logo.png" id="pngLogoL"/>
+						<img src="../images/small_logo.png" id="pngLogoL" alt="logo"/>
 						<a>Palmares AC Milan</a>
 					</h1>
 				</div>
