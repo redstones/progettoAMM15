a6caa51ff4294b559ccd7e74f7c2b82662b59b71
redstones <massilepori@gmail.com>
2016-05-10 15:29:11 +0200

    Update leftBar.php

--- php/view/cliente/leftBar.php
+++ php/view/cliente/leftBar.php
@@ -1,5 +1,5 @@
 <h2>Cliente</h2>
-<ul>
+<ul class="blocklist">
     <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : '' ?>"><a href="cliente">Home</a></li>
     <li class="<?= $vd->getSottoPagina() == 'anagrafica' ? 'current_page_item' : '' ?>"><a href="cliente/anagrafica">I tuoi dati</a></li>
     <li class="<?= $vd->getSottoPagina() == 'coppa' ? 'current_page_item' : '' ?>"><a href="cliente/coppa">Palmares Ac Milan</a></li>
