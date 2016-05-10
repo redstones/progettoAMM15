810c8fb4f2a402afe4590fa8cb7117f82600df50
redstones <massilepori@gmail.com>
2016-05-10 11:20:40 +0200

    Update anagrafica.php

--- php/view/cliente/anagrafica.php
+++ php/view/cliente/anagrafica.php
@@ -7,6 +7,7 @@
 		</ul>
 	</h2>
 	</p>
+	
 	<p>
 	<h3>Indirizzo</h3>
 			<form method="post" action="cliente/anagrafica">	
@@ -15,17 +16,17 @@
 				<label for="via">Via o Piazza:</label>
 				<input type="text" name="via" id="via" value="<?= $user->getVia() ?>"/>		
 			</p>
-			<br/>
+			<br>
 			<p>
 			<label for="civico">Numero Civico</label>
 			<input type="text" name="civico" id="civico" value="<?= $user->getNumeroCivico() ?>"/>
 			</p>
-			<br/>
+			<br>
 			<p>
 				<label for="citta">Citt&agrave;</label>
 				<input type="text" name="citta" id="citta" value="<?= $user->getCitta() ?>"/>
 			</p>
-			<br/>
+			<br>
 			<p>
 				<input type="submit" value="Salva" style ="margin-left: 150px;" class="formbutton" />
 			</p>
@@ -41,7 +42,7 @@
 				<label for="email">Email:</label>
 				<input type="text" name="email" id="email"value="<?= $user->getEmail() ?>"/>
 			</p>
-			<br/>
+			<br>
 			<p>
 				<input type="submit" value="Salva" style ="margin-left: 150px;" class="formbutton"/>
 			<p>
@@ -57,12 +58,12 @@
 					<label for="pass1">Nuova Password:</label>
 					<input type="password" name="pass1" id="pass1"/>
 				</p>
-				<br/>
+				<br>
 				<p>
 					<label for="pass2">Conferma:</label>
 					<input type="password" name="pass2" id="pass2"/>
 				</p>
-				<br/>
+				<br>
 				<p>
 					<input type="submit" value="Cambia" style ="margin-left: 150px;" class="formbutton"/>
 				</p>
